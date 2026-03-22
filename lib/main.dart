0c879c6a15407e02f067cb606e3b8abe702be177
Shin Yujin <108513967+gwonbe@users.noreply.github.com>
2026-03-22 17:06:13 +0900

    [2026.03.22.일] Hoho Fashion 시작

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+
+void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hoho Fashion',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Color(0xFFBEADD9)), // FF (불투명) + 색상
+      ),
+      home: const MyHomePage(title: 'Hoho Fashion'),
+    );
+  }
+
+}
+
+class MyHomePage extends StatefulWidget {
+
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    FlutterNativeSplash.remove();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: .center,
+          children: [
+            const Text('Hoho Fashion'),
+          ],
+        ),
+      ),
+    );
+  }
+
+}
